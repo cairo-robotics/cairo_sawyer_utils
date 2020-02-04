504f3a3d35f7cb9e982747c9a8100ee52ff1c822
Jack Kawell <jgkawell@outlook.com>
2020-02-04 16:09:16 -0700

    Update ros_install.sh

--- install_scripts/kinetic/ros_install.sh
+++ install_scripts/kinetic/ros_install.sh
@@ -8,7 +8,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt -y update
 sudo apt -y install ros-kinetic-desktop-full
-sudo rosdep init && rosdep update
+rosdep init && rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 
 # Install needed dependencies for building packages
